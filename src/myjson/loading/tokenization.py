224964cc2dfccbe27a0bd9fd1f7f7b45ebd72c70
Opemipo Ogunkola <ogunks@live.com>
2019-05-25 20:34:39 +0100

    Implement tokenization

--- src/myjson/loading/tokenization.py
+++ src/myjson/loading/tokenization.py
@@ -1,46 +1,64 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import List, Tuple
+
+from .tokens import (
+    Delimiter,
+    MappingEnd,
+    MappingStart,
+    ScalarValue,
+    Separator,
+    SequenceEnd,
+    SequenceStart,
+    Token
+)
 
 
-def tokenize(string):
-    tokens = []
-    return tokens
+def tokenize(string: str) -> List['Token']:
+    tokens: List['Token'] = []
+    currentState: 'State' = Unquoted()
+    allTokens: List['Token'] = []
+    for letter in string:
+        currentState, tokens = currentState.transition(inp=letter)
+        allTokens.extend(tokens)
+    return allTokens
 
 
-@dataclass
-class Token:
-    pass
-
-
-@dataclass
-class MappingStart(Token):
-    pass
-
-
-@dataclass
-class MappingEnd(Token):
-    pass
-
-
-@dataclass
-class Delimiter(Token):
-    pass
-
-
-@dataclass
-class SequenceStart(Token):
-    pass
-
-
-@dataclass
-class SequenceEnd(Token):
-    pass
+class State(ABC):
+    @abstractmethod
+    def transition(self, inp: str) -> Tuple['State', List['Token']]:
+        pass
 
 
 @dataclass
-class ScalarValue(Token):
-    data: str
+class Unquoted(State):
+
+    def transition(self, inp: str) -> Tuple['State', List['Token']]:
+        if inp == '{':
+            return self, [MappingStart()]
+        elif inp == '}':
+            return self, [MappingEnd()]
+        elif inp == '[':
+            return self, [SequenceStart()]
+        elif inp == ']':
+            return self, [SequenceEnd()]
+        elif inp == ':':
+            return self, [Separator()]
+        elif inp == ',':
+            return self, [Delimiter()]
+        elif inp in {' ', '\n'}:
+            return self, []
+        elif inp == '"':
+            return Quoted(), []
+        raise Exception('unknown special character')
 
 
 @dataclass
-class Separator(Token):
-    pass
+class Quoted(State):
+    data: str = ''
+    def transition(self, inp: str) -> Tuple['State', List['Token']]:
+        if inp == '"':
+            return Unquoted(), [ScalarValue(data=self.data)]
+        else:
+            self.data += inp
+            return self, []
--- src/tests/integration/myjson_test.py
+++ src/tests/integration/myjson_test.py
@@ -3,18 +3,32 @@ import json
 import pytest
 
 import myjson
+import os
 
 
-@pytest.mark.xfail
 @pytest.mark.parametrize(
     ("description", "string"),
     [
         ('test that this works for a simple mapping', '{"key": "value"}', ),
-        ('test that this works for a mapping that has a value that is a mapping', '{"key": {"key": "value"}}', ),
-        ('test that this works for a sequence of scalars', '["key", "value"]', ),
-        ('test that this works for a sequence of mappings', '[{"key": "value"}, {"key2": "value"}]', ),
-        ('test that this works for a string', '"value"', ),
+        ('test that this works for a mapping that has a value that is a mapping', '{"key": {"key": "value"}}'),
+        ('test that this works for a sequence of scalars', '["key", "value"]'),
+        ('test that this works for a sequence of mappings', '[{"key": "value"}, {"key2": "value"}]'),
+        ('test that this works for a string', '"value"'),
+        ('test that this works for a realistic json ', '"value"'),
     ]
 )
 def test_loading(description, string):
     assert myjson.loads(string) == json.loads(string)
+
+
+@pytest.mark.parametrize(
+    ("description", "filename"),
+    [
+        ('test that this works for a realistic json ', 'sample.json'),
+    ]
+)
+def test_loading_from_file(description, filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    with open(filename) as f:
+        string = f.read()
+        assert myjson.loads(string) == json.loads(string)
